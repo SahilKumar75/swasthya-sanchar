4a526f5a293a2b913fb6b472a74f6d7366c77acc
Sahil Kumar Singh <60318530+SahilKumar75@users.noreply.github.com>
2025-12-17 19:06:18 +0530

    Add migration for PatientProfile fields (fullName, profilePicture, streetAddress)

---
+++ prisma/migrations/20251217190555_add_patient_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "PatientProfile" ADD COLUMN "fullName" TEXT;
+ALTER TABLE "PatientProfile" ADD COLUMN "profilePicture" TEXT;
+ALTER TABLE "PatientProfile" ADD COLUMN "streetAddress" TEXT;
